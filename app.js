240b80dccbf2a25fbec71afb80ad4467db80d245
ArifulIslam3221 <pkmep4670@gmail.com>
2024-02-12 00:39:47 +0600

    v1||c1

---
+++ app.js
@@ -0,0 +1,123 @@
+document.getElementById('blueColor').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'blue'
+})
+document.getElementById('darkColor').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#2e3440'//#aec
+})
+document.getElementById('greenColor').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'green'
+})
+document.getElementById('indigoColor').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'indigo'
+})
+document.getElementById('orangeColor').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'orange'
+})
+document.getElementById('redColor').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'red'
+})
+document.getElementById('violetColor').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'violet'
+})
+document.getElementById('whiteColor').addEventListener('click', function(){
+   document.body.style.backgroundColor = '#fff'
+})
+document.getElementById('yellowColor').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'yellow'
+})
+//random color
+document.getElementById('random0').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#aec'
+})
+document.getElementById('random1').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#ace'
+})
+document.getElementById('random2').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#cae'
+})
+document.getElementById('random3').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#cea'
+})
+document.getElementById('random4').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#eac'
+})
+document.getElementById('random5').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#eca'
+})
+document.getElementById('random6').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#bd245f'
+})
+document.getElementById('random7').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#cff'
+})
+document.getElementById('random8').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#e4f245'
+})
+document.getElementById('random9').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#f1f2f3'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('hello js!')
+// const btn = document.getElementById('btn').style.backgroundColor = 'red'
+//         function clickForBlueBg(){
+//             document.getElementById('body').style.backgroundColor = 'blue'
+//         }
+
+// // 2 add onclick funciton//important
+//         function bgRed(){
+//             document.body.style.backgroundColor = 'red';
+//         }
+// //option 3
+// const makeBlueBtn = document.getElementById('make-blue');
+//     makeBlueBtn.onclick = makeBlue;
+//     function makeBlue(){
+//     document.body.style.backgroundColor = 'blue';
+// }
+// // 3 alt
+// const purpleBtn = document.getElementById('make-purple');
+//         purpleBtn.onclick = function makePurple(){
+//             document.body.style.backgroundColor = 'purple'
+//         }
+
+
+// //4
+// const pinkBtn = document.getElementById('make-pink');
+//         pinkBtn.addEventListener('click', makePink)
+//         function makePink(){
+//             document.body.style.backgroundColor = 'pink';
+//         }
+        
+// // 4 alt
+// const makeGreenbtn = document.getElementById('make-green');
+//         makeGreenbtn.addEventListener('click', function makeGreen(){
+//             document.body.style.backgroundColor = 'green';
+// }) 
+// //4 final// most important
+// document.getElementById('make-violet').addEventListener('click', function(){
+//     document.body.style.backgroundColor = 'violet'
+// })
